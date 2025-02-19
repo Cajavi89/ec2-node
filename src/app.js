d87b7945ed30b3a647e36c765b414368a1651f18
Cajavi89 <cajavi89@gmail.com>
2025-02-18 22:48:41 -0500

    segundo commit

--- src/app.js
+++ src/app.js
@@ -7,8 +7,12 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.get("/prueba", (req, res) => {
-  res.send({ data: "hola mundo" });
+  res.send({ data: "hola prueba" });
 });
 
 app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
